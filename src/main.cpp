8d467a21daf3f501133e009e4f0d56ffab464baa
Christopher Spring <chris@nolaspring.net>
2018-04-09 11:02:39 -0500

    coding

--- src/main.cpp
+++ src/main.cpp
@@ -32,48 +32,14 @@ void transmitReadings(){
 
 
 void requestGimbals(){
-  Wire.beginTransmission(10);
-  Wire.write(0);
-  Wire.endTransmission();
-}
-
-// this is the gimbal loop
-// catches response to a read request from the gimbal controller
-// pulls values into cortex buffer
-// delays
-/// makes another request
-void readGimbals() {
   int i = 0;
-  
+  Wire.requestFrom(10,4);
   while(Wire.available()){
     readBuffer[i] = Wire.read();
     i++;
   }
-
   Serial.println((String)readBuffer[0] + " " + (String)readBuffer[1] + " " + (String)readBuffer[2] + " " + (String)readBuffer[3]);
-  delay(delayMills);
-  requestGimbals();
 }
-
-
-
-void readSynthetics(){
-  //bit shift over 4 bytes, read pre-defined number of bytes from synthetic states
-
-}
-
-void requestSynthetics(){
-
-}
-
-void startControlLoop(){
-  // this will kick off the loop for reading the gimbals
-    requestGimbals();
- // this will kick off the loop for reading synthetic readValues
-    requestSynthetics();
-}
-
-
 
 void setup() {
   Serial.begin(9600);                // join i2c bus with address #8
@@ -83,6 +49,8 @@ void setup() {
 
 // simply send the buffers from gimbal, switch and synthetic values to the radio module via i2c
 void loop() {
+  requestGimbals();
+  delay(delayMills);
   transmitReadings();
   delay(delayMills);
   //Serial.println("YO");
